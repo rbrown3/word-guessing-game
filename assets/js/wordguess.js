50b067a0d70144ec15bdbbab547de205d5910ffe
Bad Bitch <rosannabrown@outlook.com>
2019-10-13 23:39:18 -0400

    fixed triesLeft counter, changed event listener to keyup

--- assets/js/wordguess.js
+++ assets/js/wordguess.js
@@ -1,18 +1,17 @@
 // GLOBAL VARIABLES
 //=================================================================
+var words;
+var randNum;
+var randWord;
+var triesLeft = 6;
+var unknownLetters;
+var underScores = [];
+var playerGuesses = [];
+var changeUnderScores;
+var wrongLetter = [];
 
- var currentWord = [];
- var keyword;
- var generateNumber;
- var randNum;
- var randWord;
- var rightWord = [];
- var wrongWord = [];
- var unknownLetters;
- var underscore = [];
- var underscores;
- var words;
- var losses = 0;
+
+
 
 
 
@@ -28,8 +27,6 @@
   .then(res => (res.text())) // get the response back in text
   .then(data => words = data.split(/\r|\n/)) // turn the response into an array of words
   .then(words => this.startGame())
-  .then(() => this.generateUnderScore())
-  .then(()  => this.keypressHandler())
 }
 
 getArrOfWords();
@@ -40,58 +37,59 @@ getArrOfWords();
       randWord = words[randNum];
       // currentWord.push(randWord)
       unknownLetters = Array.from(randWord);
+      console.log(unknownLetters)
+      for(var i = 0; i < unknownLetters.length; i++)
+      {
+        underScores.push('_')
+      }
+      document.getElementById('word-blanks').innerHTML = underScores.join(" ")
+      console.log(underScores);
+      // reset
+      wrongLetters = [];
+      triesLeft = 6;
+
+      //HTML
+      document.getElementById('guesses-left').innerHTML = triesLeft;
 
    }
+   //
+   // function trackWinsLosses()
+   // {
+   //
+   // }
 
-// make underscores for the length of the word
-  function generateUnderScore() {
-       underscore = [];
-       for (var i = 0; i < unknownLetters.length; i++) {
-           underscore[i] = "_";
-           var underscores = document.getElementById('word-blanks')
-           underscores.innerHTML = underscore.join(' ')
+  // listen for keyup (letters that are pressed and released)
+   document.onkeyup = function(event){
+     playerGuesses = event.key;
+     console.log(playerGuesses)
 
+     if(unknownLetters.indexOf(playerGuesses) > - 1) {
+     for(var i = 0; i < unknownLetters.length; i++)
+
+     {
+
+       if (unknownLetters[i] === playerGuesses)
+        {
+
+        underScores[i] = playerGuesses;
+        var changeUnderScores = document.getElementById("word-blanks");
+        changeUnderScores.innerHTML = underScores.join(' ');
+        document.getElementById('message').innerHTML = "CORRECT!"
+   //     correct++;
+   //     trackWinsLosses();
          }
-
-  };
-
-
-  // listen for keypress
-   // determine if key press is the correct letter or not
-   // push into the correct array of wrong or right guesses
-  function keypressHandler(){
-
-    {
-    document.addEventListener('keyup', (event) => {
-       event.preventDefault();
-
-
-        keyword = String.fromCharCode(event.which)
-       for (var i = 0; i < unknownLetters.length; i++) {
-         if (event.repeat) { return }
-         if ( unknownLetters[i] === keyword) {
-          underscore[i] = keyword;
-          var underscores = document.getElementById('word-blanks')
-          underscores.innerHTML = underscore.join(' ')
-          rightWord.push(keyword)
-          var displayRightMessage =document.getElementById("message")
-          displayRightMessage.innerHTML = 'Corrrect!';
-          var displayrightGuesses =document.getElementById("rightGuess")
-          displayrightGuesses.innerHTML = [... new Set(rightWord)].join('');
-  }
-  if (unknownLetters.indexOf(keyword) < 0){
-        wrongWord.push(keyword);
-        var displayWrongGuesses =document.getElementById("wrongGuess")
-        var displayWrongMessage =document.getElementById("message")
-        displayWrongMessage.innerHTML = 'Incorrect, please try again!';
-        displayWrongGuesses.innerHTML =  [... new Set(wrongWord)].join('');
-        losses -= 1
-        console.log(wrongWord)
-        var fail = document.getElementById('attempts')
-        fail.innerHTML = losses
-
-      }
-      }
-    })
+   //
+     }
+   //
    }
-  }
+      else
+      {
+      wrongLetter.push(playerGuesses);
+      triesLeft--;
+      document.getElementById("wrongGuess").innerHTML = wrongLetter
+      document.getElementById('guesses-left').innerHTML = triesLeft;
+      document.getElementById('message').innerHTML = "Wrong guess, try again!"
+   //    trackWinsLosses();
+      }
+
+   }
